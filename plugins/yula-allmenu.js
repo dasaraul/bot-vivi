66ae669fedf67886a35ba2400974d3775c833f75
dasaraul <tamskun29@gmail.com>
2024-12-19 00:09:20 +0700

    e

--- plugins/yula-allmenu.js
+++ plugins/yula-allmenu.js
@@ -201,7 +201,7 @@ conn.sendMessage(m.chat, {
                    title: global.info.namebot + `© 2024`,
                    body: 'Powered By Maximus',
                    thumbnailUrl: global.maximus ? global.maximus : thum,
-                   sourceUrl: pickRandom(["https://chat.whatsapp.com/HSBZ7gAskXIJ71e8iXaLSY","https://chat.whatsapp.com/HSBZ7gAskXIJ71e8iXaLSY"]),
+                   sourceUrl: pickRandom(["https://chat.whatsapp.com/LZCnnSQFPkF3C6zrDcH5n8","https://chat.whatsapp.com/EWxOwlaJXTsIhj706JsfmZ"]),
                    mediaType: 1,
                    renderLargerThumbnail: true
                    },
